69ac064d94ab0b579835a4e34fe65e76999006c9
Byteme <shvartsmand@gmail.com>
2018-06-28 13:37:49 -0700

    save

--- models/post.js
+++ models/post.js
@@ -1,3 +1,26 @@
 module.exports = function (sequelize, DataTypes) {
-    
-}
+
+    var Post = sequelize.define("Post", {
+
+        painIntensity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+        medications: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        dosage: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        notes: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+    });
+    return Post;
+};
--- models/user.js
+++ models/user.js
@@ -1,3 +1,31 @@
 module.exports = function (sequelize, DataTypes) {
-    
-}
+    var User = sequelize.define("User", {
+
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        dob: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+
+    });
+    return User;
+};
