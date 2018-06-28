65e849d4bc4381c5de06cfba2dbc907a5db5bcb5
Byteme <shvartsmand@gmail.com>
2018-06-28 14:14:10 -0700

    started working on apiRoutes-post

--- routes/api-routes-post.js
+++ routes/api-routes-post.js
@@ -3,9 +3,14 @@
 
 // Requiring the models
 var db = require("../models");
-
 // Routes
 // =============================================================
 module.exports = function (app) {
     
-}
+    app.get("/api/posts/", function(req, res){
+        db.Post.findAll({})
+        .then(function(dbPost){
+            res.json(dbPost);
+        });
+    });
+};
